dc158f99031b807f53484a87f5326ecb078ea75d
AmirAliEidivandi <amirah648887@gmail.com>
2023-01-27 00:38:31 +0330

    set libraries in index.tsx and config css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
 import "./index.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "react-lazy-load-image-component/src/effects/opacity.css";
+import "react-circular-progressbar/dist/styles.css";
+import "react-toastify/dist/ReactToastify.css";
+
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
