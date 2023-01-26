5692e8f55ff85e8ecf34fe95ceebbac93ae34263
AmirAliEidivandi <amirah648887@gmail.com>
2023-01-26 22:33:29 +0330

    mv-app/2-skeleton-common

---
+++ src/components/common/Skeleton.tsx
@@ -0,0 +1,16 @@
+import { FC, HTMLProps } from "react";
+
+interface SkeletonProps {
+    className: string;
+    children: React.ReactNode;
+}
+
+const Skeleton: FC<HTMLProps<HTMLDivElement> | SkeletonProps> = ({ className, children, ...others }) => {
+    return (
+        <div className={`${className} animate-pulse bg-dark-lighten rounded-md`} {...others}>
+            {children}
+        </div>
+    );
+};
+
+export default Skeleton;
