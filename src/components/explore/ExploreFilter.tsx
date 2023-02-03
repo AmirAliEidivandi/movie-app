15eef69964d74be5d373e80b4248450a3722d79a
AmirAliEidivandi <amirah648887@gmail.com>
2023-02-03 22:08:37 +0330

    finish ExploreFilter

---
+++ src/components/explore/ExploreFilter.tsx
@@ -0,0 +1,18 @@
+import { FunctionComponent } from "react";
+import SortBy from "./SortBy";
+import FilterBy from "./FilterBy";
+
+interface ExploreFilterProps {
+    currentTab: string;
+}
+
+const ExploreFilter: FunctionComponent<ExploreFilterProps> = ({ currentTab }) => {
+    return (
+        <>
+            <SortBy />
+            <FilterBy currentTab={currentTab} />
+        </>
+    );
+};
+
+export default ExploreFilter;
